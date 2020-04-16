f6fcec619c4cff2b6e40f753bff7645f7b68c877
Gabriel Cavalcante <35786864+gcavalcantes@users.noreply.github.com>
2020-04-15 22:10:40 -0300

    Script de Expressões Matemárica (#3)
    * Cena menu principal
    
    Criada a cena base para o menu principal.
    
    * Template de menu principal
    
    Menu simples para referência. Pode ser substituído a qualquer momento.
    
    * Mudanças no README
    
    Será expandido no futuro.
    
    * Pastas de imagens e prefabs
    
    Adicionadas pastas de prefabs e imagens.
    Adicionado um script (vazio) para lidar com as expressões matemáticas.
    
    * Adição das expressões matemáticas.
    
    Algumas expressões faltando.
    Falta implementar como o jogo vai lidar com essas expressões.
    
    * Expressões adicionadas
    
    Expressões adicionadas. Mais podem ser adicionadas a qualquer momento.
    
    Co-authored-by: João Marques <jotaveultra@gmail.com>

---
+++ Assets/Scripts/math_exp.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class math_exp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Lista de expressões que são utilizados
+    List<string> expressao1 = new List<string>
+    {
+        "8", "+", "8", "=", "16"
+    };
+    List<string> expressao2 = new List<string>
+    {
+        "1000", "-", "438", "=", "562"
+    };
+    List<string> expressao3 = new List<string>
+    {
+        "789", "*", "11", "=", "8679"
+    };
+    List<string> expressao4 = new List<string>
+    {
+        "450", "/", "50", "=", "9"
+    };
+    List<string> expressao5 = new List<string>
+    {
+        "20", "-", "373", "=", "-353"
+    };
+    List<string> expressao6 = new List<string>
+    {
+        "999", "/", "9", "=", "111"
+    };
+    List<string> expressao7 = new List<string>
+    {
+        "360", "/", "9", "=", "4"
+    };
+    List<string> expressao8 = new List<string>
+    {
+        "600", "/", "30", "=", "562"
+    };
+    List<string> expressao9 = new List<string>
+    {
+        "613", "-", "605", "=", "8"
+    };
+    List<string> expressao10 = new List<string>
+    {
+        "94", "*", "13", "=", "562"
+    };
+
+}
