4c8596aa53a22a1e42f23a3f8b22101a41d78608
Gabriel Cavalcante <gabrielcavalcante1021@gmail.com>
2020-04-17 00:21:10 -0300

    Vários scripts criados.
    Manager do jogo, do áudio e de cenas criados. Nenhum funcional por enquanto.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int pontuacao;
+    //Variável para mostrar a pontuação na tela de jogo
+    public Text textPontuacao;
+    // Variável para mostrar a pontuação final na tela de game over
+    public Text textPontuacaoFinal;
+    public bool jogoPausado;
+    public GameObject optionPanel;
+    // Mostra painel de Game over
+    public GameObject gameOverPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Variável para a pontuação
+        pontuacao = 0;
+        // Mostra o resultado do jogador na caixa de texto
+        textPontuacao.text = pontuacao.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            janelaOpcoes();
+        }
+    }
+
+    // Aumenta a pontuação do jogador.
+    public void aumentaPontuacao()
+    {
+        pontuacao++;
+        textPontuacao.text = pontuacao.ToString();
+    }
+
+    // Método da janela de opções (em jogo)
+    public void janelaOpcoes()
+    {
+        if (jogoPausado)
+        {
+            jogoPausado = false;
+            // Desativa a tela de opções
+            optionPanel.SetActive(false);
+        } else {
+            jogoPausado = true;
+            // Ativa a tela de opções
+            optionPanel.SetActive(true);
+        }
+        
+    }
+
+    // Método para reiniciar jogo
+    public void reiniciarJogo(){
+        //SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+    }
+}
