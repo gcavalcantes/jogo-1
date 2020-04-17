531833192bed56f8f2d6ac43d84e7a5fff4d0ae3
João Marques <jotaveultra@gmail.com>
2020-04-16 23:51:52 -0300

    Tela de creditos

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject menuInicial;
+    public GameObject menuCredito;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void abrirCreditos()
+    {
+        menuInicial.SetActive(false);
+        menuCredito.SetActive(true);
+    }
+
+    public void fecharCredito()
+    {
+        menuInicial.SetActive(true);
+        menuCredito.SetActive(false);
+    }
+}
